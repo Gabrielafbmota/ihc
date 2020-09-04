2ae6160a975a7c102ba70b3012cfeac21971a466
Gabriela <gabriela.mota@stitdata.com>
2020-09-04 17:07:55 -0300

    up

---
+++ build/precache-manifest.ca48c850f5fb971f964ef27376083be8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24c0d32641f68b4081543ddbf0fcd11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a396280ee473312cfd5",
+    "url": "/static/js/2.12a63b21.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.12a63b21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4dbb0846fc90aaa8cdc",
+    "url": "/static/js/main.94bc878f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
