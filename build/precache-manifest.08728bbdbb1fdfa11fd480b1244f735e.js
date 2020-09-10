0cde080550d02399d8de74c2146e2884005b7123
Gabriela <gabrielamota.unicamp@gmail.com>
2020-09-10 17:47:36 -0300

    'up'

---
+++ build/precache-manifest.08728bbdbb1fdfa11fd480b1244f735e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc12c2fe81fbd05705f4234ea9057e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6145a32c843e98fc2049",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ab6c32ff559d022f433c",
+    "url": "/static/css/main.80a343d6.chunk.css"
+  },
+  {
+    "revision": "6145a32c843e98fc2049",
+    "url": "/static/js/2.4a3d8f46.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4a3d8f46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab6c32ff559d022f433c",
+    "url": "/static/js/main.d470914f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.20976be4bce7af51054f70d10e909557.js"
+  "/precache-manifest.08728bbdbb1fdfa11fd480b1244f735e.js"
 );
 
 self.addEventListener('message', (event) => {
